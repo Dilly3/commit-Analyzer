0a219831d6635da4d0502a08dfe0df0ac4807a7e
michael anikammadu <dil.anikamadu@gmail.com>
2024-07-14 18:17:06 +0100

    storage and cron

--- go.mod
+++ go.mod
@@ -14,6 +14,8 @@ require (
 )
 
 require (
+	github.com/go-co-op/gocron v1.37.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -22,7 +24,9 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/dilly3/houdini/config"
-	"github.com/dilly3/houdini/github"
-	"github.com/dilly3/houdini/server"
-	"github.com/dilly3/houdini/storage"
+	"github.com/dilly3/houdini/api/server"
+	"github.com/dilly3/houdini/internal/config"
+	"github.com/dilly3/houdini/internal/storage"
+	"github.com/dilly3/houdini/pkg/cron"
+	"github.com/dilly3/houdini/pkg/github"
 	"github.com/rs/zerolog"
 	"net/http"
 	"os"
@@ -19,6 +20,10 @@ func main() {
 	storage.New(config.Config, &logger)
 	github.DefaultGHClient = github.NewGHClient(config.Config)
 	handler := server.NewHandler(&logger)
+	cron.InitCron()
+	cron.SetCronJob(github.DefaultGHClient.GetCommitsCron, cron.GetTimeDuration())
+	cron.SetCronJob(github.DefaultGHClient.GetRepoCron, cron.GetTimeDuration())
+	cron.StartCronJob()
 	httpHandler := server.NewMuxRouter(handler, time.Minute)
 	httpServer := &http.Server{
 		Addr:    config.Config.Port,
