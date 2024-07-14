e211c64b24454d14e48e0fa1d02e5de6209e31bf
michael anikammadu <dil.anikamadu@gmail.com>
2024-07-14 21:01:25 +0100

    comments

--- pkg/cron/cron.go
+++ pkg/cron/cron.go
@@ -8,28 +8,38 @@ import (
 
 var schedular *gocron.Scheduler
 
-// time in minutes
-var timeDuration int64 = 3
+// time in minutes (1 hour)
+var timeDuration int64 = 60
 
+// SetTimeDuration sets the time delay for the cron job
 func SetTimeDuration(t int64) {
 	timeDuration = t
 
 }
+
+// GetTimeDuration returns the time delay for the cron job
 func GetTimeDuration() int64 {
 	return timeDuration
 }
 
+// InitCron initializes the gocron scheduler
 func InitCron() {
 
 	s := gocron.NewScheduler(time.UTC)
 	schedular = s
 }
+
+// StopCronJob stops the cron jobs
 func StopCronJob() {
 	schedular.Stop()
 }
+
+// StartCronJob starts the cron jobs
 func StartCronJob() {
 	schedular.StartAsync()
 }
+
+// SetCronJob sets the cron job
 func SetCronJob(f func() error, interval int64) {
 	timeD := time.Duration(interval) * time.Minute
 	_, err := schedular.Every(timeD).Do(f)
