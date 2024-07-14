3d0f581cd618134e64b9186d050325c18be43639
michael anikammadu <dil.anikamadu@gmail.com>
2024-07-14 18:21:49 +0100

    storage update

--- internal/storage/IStore.go
+++ internal/storage/IStore.go
@@ -8,23 +8,19 @@ func GetDefaultStore() IStore {
 
 type IStore interface {
 	ICommitStore
-	IAuthorStore
 	IRepoStore
 }
 
 type Store struct {
 	ICommitStore
-	IAuthorStore
 	IRepoStore
 }
 
 func NewStore(storage *Storage) Store {
 	ics := NewCommitStore(storage)
-	ias := NewAuthorStore(storage)
 	irs := NewRepoStore(storage)
 	store := Store{
 		ICommitStore: ics,
-		IAuthorStore: ias,
 		IRepoStore:   irs,
 	}
 	defaultStore = store
