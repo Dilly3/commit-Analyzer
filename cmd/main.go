e59c00a9022eaa75762d617324d236f8ca7e79b8
michael anikammadu <dil.anikamadu@gmail.com>
2024-07-23 05:33:57 +0100

    main

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/dilly3/houdini/internal/config"
 	ghi "github.com/dilly3/houdini/internal/github"
-	"github.com/dilly3/houdini/internal/model"
 	"github.com/dilly3/houdini/internal/repository"
+	"github.com/dilly3/houdini/internal/repository/cache"
 	"github.com/dilly3/houdini/internal/server"
 	"github.com/dilly3/houdini/pkg/cron"
 	"github.com/dilly3/houdini/pkg/github"
 	"github.com/dilly3/houdini/storage/postgres"
+	"github.com/dilly3/houdini/storage/redis"
 	"github.com/rs/zerolog"
 	"net/http"
 	"os"
@@ -23,9 +24,13 @@ func main() {
 	c := config.Config
 	pgs := postgres.New(c, &logger)
 	repository.NewStore(pgs)
-	model.SetOwnerName(c.GithubOwner)
-	model.SetRepoName(c.GithubRepo)
-	model.SetSince(c.GithubSince)
+	settings := config.GetSettings()
+	redisClt, err := redis.NewRedisClient(c.RedisADDR, c.RedisUser, c.RedisPassword, settings, &logger)
+	if err != nil {
+		logger.Error().Err(err).Msgf("redis connection: %s", err.Error())
+		os.Exit(1)
+	}
+	cache.NewCache(redisClt)
 	githubClient := github.NewGHClient(c.GithubBaseURL, c.GithubToken)
 	gitHubInteract := ghi.NewGHubITR(githubClient)
 	handler := server.NewHandler(&logger)
