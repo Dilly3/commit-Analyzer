34fd460b80b24a440d2f80ebacb307c8160eef79
michael anikammadu <dil.anikamadu@gmail.com>
2024-07-23 05:31:35 +0100

    update

--- internal/github/helper.go
+++ internal/github/helper.go
@@ -3,7 +3,6 @@ package github
 import (
 	"github.com/dilly3/houdini/internal/model"
 	"github.com/dilly3/houdini/pkg/github"
-	"strconv"
 	"strings"
 )
 
@@ -47,14 +46,4 @@ func mapRepoResponse(repo *github.RepoResponse) model.RepoInfo {
 		Stars:       repo.Stars,
 		OpenIssues:  repo.OpenIssues,
 	}
-}
-func stringToInteger(s string) int {
-	if s == "" {
-		return 0
-	}
-	num, err := strconv.Atoi(s)
-	if err != nil {
-		return 0
-	}
-	return num
 }
